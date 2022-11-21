256a8eee2ef9db16216fd1ea28c6eb5d9bbac492
yashvanth5 <116138513+yashvanth5@users.noreply.github.com>
2022-11-21 13:38:36 +0530

    js imported

---
+++ app.js
@@ -0,0 +1,10 @@
+// console.log("heloo bruh");
+
+
+// var userName = prompt ("what was your name? ");
+// var welcomeMsg = "weolcome " + userName ;
+// alert (welcomeMsg);
+
+
+var buttonTranslate = document.querySelector("#btn-translate");
+console.log (buttonTranslate);
